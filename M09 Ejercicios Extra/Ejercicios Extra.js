786066b64ec35666336dacda9cc8da290b379d4d
Rocioubillos <110296802+Rocioubillos@users.noreply.github.com>
2023-04-09 17:20:34 -0300

    Add files via upload

---
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -0,0 +1,119 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+
+function deObjetoAarray(objeto) {
+   // Recibes un objeto. Tendrás que crear un arreglo de arreglos.
+   // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
+   // Estos elementos debe ser cada par clave:valor del objeto recibido.
+   // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
+   // Tu código:
+const array=[]  
+array.push(Object.entries(objeto));
+return Object.entries(objeto)
+}
+
+function numberOfCharacters(string) {
+   // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
+   // letras del string, y su valor es la cantidad de veces que se repite en el string.
+   // Las letras deben estar en orden alfabético.
+   // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
+   // Tu código:
+   var objeto ={};
+   for (let i=0; i < string.length; i++) {
+      objeto[string[i]] = objeto[string[i]] + 1 ||1;
+   }
+   return objeto;
+}
+
+function capToFront(string) {
+   // Recibes un string con algunas letras en mayúscula y otras en minúscula.
+   // Debes enviar todas las letras en mayúscula al comienzo del string.
+   // Retornar el string.
+   // [EJEMPLO]: soyHENRY ---> HENRYsoy
+   // Tu código:
+   var may = ""
+ var min = ""
+for (var i = 0; i < string.length; i++) {
+       if( string[i] === string[i].toLowerCase()){
+            min += string[i]
+          }
+        else {
+            may += string[i]
+    }
+     }  return may + min
+}
+
+function asAmirror(frase) {
+   // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
+   // La diferencia es que cada palabra estará escrita al inverso.
+   // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
+   // Tu código:
+   let separatePhrase = frase.split(' ');
+   let phraseMirror = '';
+   for (let i = 0; i < separatePhrase.length; i++) {
+      phraseMirror += `${separatePhrase[i].split('').reverse().join('')} `
+   }
+   return phraseMirror.slice(0, -1);
+} 
+
+function capicua(numero) {
+   // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
+   // Caso contrario: "No es capicua".
+   // Tu código:
+   let num = numero.toString();
+   for (let i = 0; i < num.length; i++) {
+     return (num[i] !== num[num.length -1-i])
+     ?'No es capicua'
+     :'Es capicua';
+   }
+   
+}
+
+function deleteAbc(string) {
+   // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
+   // Retorna el string sin estas letras.
+   // Tu código:
+      return string.split("").filter(val=>{if(val!=="a"&&val!=="b"&&val!=="c")return val}).join("")
+   
+}
+
+function sortArray(arrayOfStrings) {
+   // Recibes un arreglo de strings.
+   // Debe retornar un nuevo arreglo, pero con las palabras ordenadas en orden creciente a partir
+   // de la longitud de cada string.
+   // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
+   // Tu código:
+   let nuevo = arrayOfStrings.sort((a, b) => {
+		return a.length - b.length;
+});
+ return nuevo;
+}
+
+function buscoInterseccion(array1, array2) {
+   // Recibes dos arreglos de números.
+   // Debes retornar un nuevo arreglo en el que se guarden los elementos en común entre ambos arreglos.
+   // [EJEMPLO]: [4,2,3] U [1,3,4] = [3,4].
+   // Si no tienen elementos en común, retornar un arreglo vacío.
+   // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
+   // Tu código:
+   var enComun=[]
+      for (var i = 0; i<array1.length; i++) {
+      for (var i2 = 0; i2<array2.length; i2++) {
+         if (array1[i]==array2[i2]){
+            enComun.push(array1[i])
+         }  
+      }
+   };
+   return enComun;
+}
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   deObjetoAarray,
+   numberOfCharacters,
+   capToFront,
+   asAmirror,
+   capicua,
+   deleteAbc,
+   sortArray,
+   buscoInterseccion,
+};
